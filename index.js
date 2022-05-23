03092a09a3bc5e98181047409dffecbb43cf8aa4
mjaumi <mjaumi2864@gmail.com>
2022-05-23 14:21:24 +0600

    GET API to search by id done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -27,9 +27,17 @@ async function run() {
         // GET API to get first 6 parts
         app.get('/parts', async (req, res) => {
             const query = {};
-            const parts = await partCollection.find(query).limit(6).toArray();
-            res.send(parts);
+            const firstSixParts = await partCollection.find(query).limit(6).toArray();
+            res.send(firstSixParts);
         });
+
+        // GET API to get any parts by id
+        app.get('/parts/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const parts = await partCollection.findOne(query);
+            res.send(parts);
+        })
 
     } finally {
 
