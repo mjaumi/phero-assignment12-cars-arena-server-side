299cb43e8f60d9806d6abace992055773e78362a
mjaumi <mjaumi2864@gmail.com>
2022-05-26 02:23:18 +0600

    JWT added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
@@ -9,6 +10,23 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
+
+// verifying JWT here
+const verifyJWT = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'Unauthorized Access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden Access' });
+        }
+
+        req.decoded = decoded;
+        next();
+    });
+}
 
 // MongoDB Connection
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.l4rez.mongodb.net/?retryWrites=true&w=majority`;
@@ -28,6 +46,24 @@ async function run() {
 
         console.log('MongoDB Connected');
 
+        /**
+        * -----------------------
+        * AUTHENTICATION API
+        * -----------------------
+        */
+        app.post('/getToken', async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
+            });
+            res.send({ accessToken });
+        });
+
+        /**
+         * ------------------------
+         * SERVICES API
+         * ------------------------
+         */
         // GET API to get first 6 parts
         app.get('/topSixParts', async (req, res) => {
             const query = {};
